5bcc4e6b68b67726a22d0a880f79f020b87956d4
Evergood Kim <youngsun0401@naver.com>
2024-01-28 01:53:05 +0900

    공백 파싱 구현

--- Jogoson.java
+++ Jogoson.java
@@ -1,27 +1,60 @@
 public class Jogoson {
 
-    public Object parseValue(String s){
+    public Object parseValue( String s ){
         return parseValue(new StringToBeParsed(s));
     }
 
-    public Object parseValue(StringToBeParsed s){
+    public Object parseValue( StringToBeParsed s ){
         parseWhitespace(s);
         // TODO value 알맹이
         parseWhitespace(s);
 
+        //// 남아있으면 에러
+        if( s.notDone() ){
+            throw new RuntimeException("surplus characters after a valid json");// TODO err
+        }
+
         return null;// TODO
     }
 
-    public void parseWhitespace(StringToBeParsed s){
-        // TODO
+    public void parseWhitespace( StringToBeParsed s ){
+        boolean b = true;
+        while( b && s.notDone() ){
+            char c = s.pop();
+            switch( c ){
+            case ' ':
+            case '\t':
+            case '\n':
+            case '\r':
+                break;
+            default:
+                s.back();
+                b = false;
+            }
+        }
     }
 
     private class StringToBeParsed {
-        int index = 0;// 이 위치를 처리할 차례
-        char[] cs;
+        private int i = 0;// 이 위치를 처리할 차례
+        private final char[] cs;
 
-        public StringToBeParsed(String jsonString){
+        public StringToBeParsed( String jsonString ){
             cs = jsonString.toCharArray();
+        }
+
+        public boolean notDone(){
+            if( i < cs.length )
+                return true;
+            else
+                return false;
+        }
+
+        public char pop(){
+            return cs[i++];
+        }
+
+        public void back(){
+            i -= 1;
         }
     }
 }
--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Jogoson Jogosonimnida");
 
-        String str = "  \"str\"  ";
+        String str = "  \n\r\t \t ";
 
         Jogoson j = new Jogoson();
         Object o = j.parseValue(str);
